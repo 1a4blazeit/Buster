5b661bf55a123e5678fb36d33327930fc2b9c0f8
1a4blazeit <d.y.charpen@gmail.com>
2017-01-31 12:58:23 -0500

    added vertical collision/jumping. some wonkiness to fix remains

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	int move;
+	int move; //-1 is moving left, 0 is no horizontal movement, 1 is moving right
+	int jump; //0 is grounded, 1 is jumping on next FixedUpdate, -1 is in mid-air
 	static float gravity = -9.81f;
 	static float speed = 0.15f;
 	float horizSpeed; //horizontal movement per fixedUpdate tick
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		move = 0; 
+		jump = 0;
 		horizSpeed = 0;
 		vertiSpeed = 0;
 	}
@@ -22,18 +24,20 @@ public class PlayerController : MonoBehaviour {
 		if(Input.GetKey("left")) move = -1;
 		else if(Input.GetKey("right")) move = 1;
 		else move = 0;
+		
+		if(Input.GetKeyDown("z") && jump == 0) jump = 1;
 	}
 	
 	// FixedUpdate is called more often than Update and on a more consistent clock
 	void FixedUpdate() {
 		//find the edges of the bounding box post movement. by making them slightly smaller than unit it lets them sneak through gaps without affecting size in a meaningful way
-		if(move == 1) {
-			horizSpeed = speed;
-		}
-		else if(move == -1) {
-			horizSpeed = -speed;
-		}
+		if(move == 1) horizSpeed = speed;
+		else if(move == -1) horizSpeed = -speed;
 		else horizSpeed = 0f;
+		
+		if(jump == 1) vertiSpeed = 0.3f;
+		else if (jump == 0) vertiSpeed = 0f;
+		
 		left = transform.position.x - 0.49f + horizSpeed;
 		right = transform.position.x + 0.49f + horizSpeed;
 		up = transform.position.y + 0.99f + vertiSpeed;
@@ -45,35 +49,52 @@ public class PlayerController : MonoBehaviour {
 		int rowsIntersected = maxVerti - minVerti + 1;
 		int colsIntersected = maxHori - minHori + 1;
 		GameObject[,] tileMap = GameObject.Find("EnvironmentModel").GetComponent<EnvironmentController>().tileArray;
-		bool unobstructed = true;
 		
-		
+		//handle horizontal movement: 1 means right key is being held, -1 means left is, 0 means neither
+		float maxMove = horizSpeed;
 		if(move == 1) {
-			float maxMove = horizSpeed;
-			
 			for(int i = 0; i < rowsIntersected; i++) {
 				if(tileMap[minVerti + i, maxHori].tag == "Obstacle") {
-					unobstructed = false;
-					maxMove = Mathf.Min(maxMove, (tileMap[minVerti + i, maxHori].transform.position.x - 0.49f) - (transform.position.x + 0.49f));
+					maxMove = Mathf.Min(maxMove, (tileMap[minVerti + i, maxHori].transform.position.x - 0.5f) - (transform.position.x + 0.49f));
 				}
 			}
-			if(unobstructed) transform.position += new Vector3(horizSpeed, 0, 0);
-			else transform.position += new Vector3(maxMove, 0, 0);
-				
-			
+			transform.position += new Vector3(maxMove, 0, 0);	
 		}
 		else if (move == -1) {
-			float maxMove = horizSpeed;
-			
 			for(int i = 0; i < rowsIntersected; i++) {
 				if(tileMap[minVerti + i, minHori].tag == "Obstacle") {
-					unobstructed = false;
-					maxMove = Mathf.Max(maxMove, (tileMap[minVerti + i, minHori].transform.position.x + 0.49f) - (transform.position.x - 0.49f));
+					maxMove = Mathf.Max(maxMove, (tileMap[minVerti + i, minHori].transform.position.x + 0.5f) - (transform.position.x - 0.49f));
 				}
 			}
-			if(unobstructed) transform.position += new Vector3(horizSpeed, 0, 0);
-			else transform.position += new Vector3(maxMove, 0, 0);
+			transform.position += new Vector3(maxMove, 0, 0);
 		}
-		else return;
+		
+		//handle vertical movement: apply jump impulse if applicable, then check for obstruction/move, then modify vertiSpeed according to gravity
+		
+		maxMove = vertiSpeed;
+		if(vertiSpeed > 0) {
+			jump = -1;
+			for(int i = 0; i < colsIntersected; i++) {
+				if(tileMap[maxVerti, minHori + i].tag == "Obstacle") {
+					maxMove = Mathf.Min(maxMove, (tileMap[maxVerti, minHori + i].transform.position.y - 0.5f) - (transform.position.y + 0.99f));
+				}
+			}
+			transform.position += new Vector3(0, maxMove, 0);
+			if (maxMove <= 0) jump = 0;
+		}
+		else {
+			jump = -1;
+			for(int i = 0; i < colsIntersected; i++) {
+				if(tileMap[minVerti, minHori + i].tag == "Obstacle") {
+					maxMove = Mathf.Max(maxMove, (tileMap[minVerti, minHori + i].transform.position.y + 0.5f) - (transform.position.y - 1.0f));
+				}
+			}
+			transform.position += new Vector3(0, maxMove, 0);
+			if (maxMove >= 0) jump = 0;
+		}
+		
+		
+		vertiSpeed += gravity * 0.01f;
+		
 	}
 }
