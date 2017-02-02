a296b7d534ba45012d8263a422e62d23c6d38e74
1a4blazeit <d.y.charpen@gmail.com>
2017-02-01 19:41:35 -0500

    added vertical movement, cleaned up code, no gravity yet

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -25,7 +25,11 @@ public class PlayerController : MonoBehaviour {
 		else if(Input.GetKey("right")) move = 1;
 		else move = 0;
 		
-		if(Input.GetKeyDown("z") && jump == 0) jump = 1;
+		if(Input.GetKey("up")) jump = 1;
+		else if(Input.GetKey("down")) jump = -1;
+		else jump = 0;
+		
+		//if(Input.GetKeyDown("z") && jump == 0) jump = 1;
 	}
 	
 	// FixedUpdate is called more often than Update and on a more consistent clock
@@ -35,13 +39,17 @@ public class PlayerController : MonoBehaviour {
 		else if(move == -1) horizSpeed = -speed;
 		else horizSpeed = 0f;
 		
-		if(jump == 1) vertiSpeed = 0.3f;
-		else if(jump == 0) vertiSpeed = 0f;
+		if(jump == 1) vertiSpeed = speed;
+		else if(jump == -1) vertiSpeed = -speed;
+		else vertiSpeed = 0f;
 		
-		left = transform.position.x - 0.45f + horizSpeed;
-		right = transform.position.x + 0.45f + horizSpeed;
-		up = transform.position.y + 0.95f + vertiSpeed;
-		down = transform.position.y - 1f + vertiSpeed;
+		/*if(jump == 1) vertiSpeed = 0.3f;
+		else if(jump == 0) vertiSpeed = 0f;*/
+		
+		left = transform.position.x - 0.45f;
+		right = transform.position.x + 0.45f;
+		up = transform.position.y + 0.95f;
+		down = transform.position.y - 1f;
 		int maxHori = Mathf.FloorToInt(right); //these are the coordinates of the squares (in array) that the box intersects)
 		int minHori = Mathf.FloorToInt(left);
 		int maxVerti = Mathf.FloorToInt(up);
@@ -54,22 +62,52 @@ public class PlayerController : MonoBehaviour {
 		float maxMove = horizSpeed;
 		if(move == 1) {
 			for(int i = 0; i < rowsIntersected; i++) {
-				if(tileMap[minVerti + i, maxHori].tag == "Obstacle") {
-					maxMove = Mathf.Min(maxMove, (tileMap[minVerti + i, maxHori].transform.position.x - 0.5f) - (transform.position.x + 0.45f));
+				if(tileMap[minVerti + i, maxHori].tag == "Obstacle") { //if bounding box overlaps with obstacle, can't move further!
+					maxMove = 0;
+				}
+				else if(tileMap[minVerti + i, maxHori + 1].tag == "Obstacle") {
+					maxMove = Mathf.Min(maxMove, (tileMap[minVerti + i, maxHori + 1].transform.position.x - 0.5f) - (transform.position.x + 0.5f));
 				}
 			}
 			transform.position += new Vector3(maxMove, 0, 0);	
 		}
 		else if (move == -1) {
 			for(int i = 0; i < rowsIntersected; i++) {
-				if(tileMap[minVerti + i, minHori].tag == "Obstacle") {
-					maxMove = Mathf.Max(maxMove, (tileMap[minVerti + i, minHori].transform.position.x + 0.5f) - (transform.position.x - 0.45f));
+				if(tileMap[minVerti + i, minHori].tag == "Obstacle") { //
+					maxMove = 0;
+				}
+				else if(tileMap[minVerti + i, minHori - 1].tag == "Obstacle") {
+					maxMove = Mathf.Max(maxMove, (tileMap[minVerti + i, minHori - 1].transform.position.x + 0.5f) - (transform.position.x - 0.5f));
 				}
 			}
 			transform.position += new Vector3(maxMove, 0, 0);
 		}
 		
-		//handle vertical movement: apply jump impulse if applicable, then check for obstruction/move, then modify vertiSpeed according to gravity
+		maxMove = vertiSpeed;
+		if(jump == 1) {
+			for(int i = 0; i < colsIntersected; i++) {
+				if(tileMap[maxVerti, minHori + i].tag == "Obstacle") {
+					maxMove = 0;
+				}
+				else if(tileMap[maxVerti + 1, minHori + i].tag == "Obstacle") {
+					maxMove = Mathf.Min(maxMove, (tileMap[maxVerti + 1, minHori + i].transform.position.y - 0.5f) - (transform.position.y + 1.0f));
+				}
+			}
+			transform.position += new Vector3(0, maxMove, 0);
+		}
+		else if (jump == -1) {
+			for(int i = 0; i < colsIntersected; i++) {
+				if(tileMap[minVerti, minHori + i].tag == "Obstacle") {
+					maxMove = 0;
+				}
+				else if(tileMap[minVerti - 1, minHori + i].tag == "Obstacle") {
+					maxMove = Mathf.Max(maxMove, (tileMap[minVerti - 1, minHori + i].transform.position.y + 0.5f) - (transform.position.y - 1.0f));
+				}
+			}
+			transform.position += new Vector3(0, maxMove, 0);
+		}
+		
+		
 	
 		
 		
